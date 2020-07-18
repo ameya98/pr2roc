bbe15f3bb609513ec30240284877e3c5b5d52e24
Ameya Daigavane <ameya.d.98@gmail.com>
2020-07-19 05:01:06 +0530

    Add initial tests for roc2pr.

---
+++ test/test_curve.py
@@ -0,0 +1,61 @@
+from __future__ import division
+
+from roc2pr import Curve
+
+import numpy as np
+import pytest
+from pytest import approx
+
+def check_curve(curve):
+    assert curve is not None
+
+    assert curve.points()[0] == (0, 1)
+    assert curve.points()[4] == (8, 3)
+
+    resampled_curve = curve.resample(2)
+    assert resampled_curve.points()[0] == approx((0, 1))
+    assert resampled_curve.points()[1] == approx((8, 3))
+
+    resampled_curve = curve.resample(3)
+    assert resampled_curve.points()[0] == approx((0, 1))
+    assert resampled_curve.points()[1] == approx((4, 9.5))
+    assert resampled_curve.points()[2] == approx((8, 3))
+
+    resampled_curve = curve.resample(4)
+    assert resampled_curve.points()[0] == approx((0, 1))
+    assert resampled_curve.points()[1] == approx((8/3, 49/6))
+    assert resampled_curve.points()[2] == approx((16/3, 83/9))
+    assert resampled_curve.points()[3] == approx((8, 3))
+
+    resampled_curve = curve.resample(5)
+    assert resampled_curve.points()[0] == approx((0, 1))
+    assert resampled_curve.points()[1] == approx((2, 13/2))
+    assert resampled_curve.points()[2] == approx((4, 19/2))
+    assert resampled_curve.points()[3] == approx((6, 23/3))
+    assert resampled_curve.points()[4] == approx((8, 3))
+
+def test_sorted():
+    x_vals = [0, 1, 3, 5, 8]
+    y_vals = [1, 4, 9, 10, 3]
+    points = zip(x_vals, y_vals)
+
+    curve = Curve(points, label='Test Curve')
+    check_curve(curve)
+
+def test_unsorted():
+    x_vals = [8, 1, 5, 3, 0]
+    y_vals = [3, 4, 10, 9, 1]
+    points = zip(x_vals, y_vals)
+    
+    curve = Curve(points, label='Test Curve')
+    check_curve(curve)
+
+def test_np_array():
+    x_vals = [8, 1, 5, 3, 0]
+    y_vals = [3, 4, 10, 9, 1]
+    points = np.zeros((5, 2))
+    points[:, 0] = x_vals
+    points[:, 1] = y_vals
+    
+    curve = Curve(points, label='Test Curve')
+    check_curve(curve)
---
+++ test/test_pr_curve.py
@@ -0,0 +1,59 @@
+from roc2pr import PRCurve
+
+import numpy as np
+import pytest
+from pytest import approx
+
+def test_pr_curve_reconversion():
+    rec_vals = [0.25, 0.30, 0.35, 0.40, 0.45, 0.50]
+    prec_vals = [0.5, 0.375, 0.318, 0.286, 0.265, 0.250]
+    points = zip(rec_vals, prec_vals)
+
+    pr_curve = PRCurve(points, 20/2000, label='Test PR Curve')
+    orig_points = pr_curve.points()
+    reconverted_points = pr_curve.to_roc().to_pr().points()
+
+    assert np.array(orig_points).flatten() == approx(np.array(reconverted_points).flatten())
+
+def test_pr_curve_reconversion_edge_cases():
+
+    # Precision > 0 when Recall = 0.
+    with pytest.raises(ValueError):
+        rec_vals = [0.0, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 1.0]
+        prec_vals = [0.9, 0.5, 0.375, 0.318, 0.286, 0.265, 0.250, 0.01]
+        points = zip(rec_vals, prec_vals)
+        pr_curve = PRCurve(points, 20/2000, label='Test PR Curve')
+
+    # Precision = 0 when Recall > 0.
+    with pytest.raises(ValueError):
+        rec_vals = [0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 1.0]
+        prec_vals = [0.5, 0.375, 0.318, 0.286, 0.265, 0.250, 0.0]
+        points = zip(rec_vals, prec_vals)
+        pr_curve = PRCurve(points, 20/2000, label='Test PR Curve')
+
+    # Precision = 0 when Recall = 0. This is acceptable.
+    rec_vals = [0.0, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 1.0]
+    prec_vals = [0.0, 0.5, 0.375, 0.318, 0.286, 0.265, 0.250, 0.01]
+    points = zip(rec_vals, prec_vals)
+    pr_curve = PRCurve(points, 20/2000, label='Test PR Curve')
+
+    orig_points = pr_curve.points()
+    reconverted_points = pr_curve.to_roc().to_pr().points()
+
+    assert np.array(orig_points).flatten() == approx(np.array(reconverted_points).flatten())
+
+def test_pr_curve_resample():
+    rec_vals = [0.25, 0.5]
+    prec_vals = [0.5, 0.25]
+    points = zip(rec_vals, prec_vals)
+    pr_curve = PRCurve(points, 20/2000, label='Test PR Curve')
+
+    points = pr_curve.resample(num_points=6).points()
+    rec_vals_sampled = [point[0] for point in points]
+    prec_vals_sampled = [point[1] for point in points]
+    rec_vals_expected = [0.25, 0.30, 0.35, 0.40, 0.45, 0.50]
+    prec_vals_expected = [0.5, 0.375, 0.318, 0.286, 0.265, 0.250]
+
+    assert rec_vals_sampled == approx(rec_vals_expected, abs=1e-3)
+    assert prec_vals_sampled == approx(prec_vals_expected, abs=1e-3)
+
