791c522a6ac4eeae6f1348aca97559fe554d8d19
Ameya Daigavane <ameya.d.98@gmail.com>
2020-07-19 22:46:33 +0530

    Remove try-except because this works on Python2.7 too.

--- roc2pr/__init__.py
+++ roc2pr/__init__.py
@@ -1,8 +1,3 @@
-try:
-    from .curve import Curve
-    from .pr_curve import PRCurve
-    from .roc_curve import ROCCurve
-except ImportError:
-    from curve import Curve
-    from pr_curve import PRCurve
-    from roc_curve import ROCCurve
+from .curve import Curve
+from .pr_curve import PRCurve
+from .roc_curve import ROCCurve
